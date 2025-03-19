b831b57e3518a9c2b1925752ee6e79591b4a82ae
Muslim Hussaini <hussainimuslim2@gmail.com>
2025-03-19 18:29:42 -0400

    hi

--- script.js
+++ script.js
@@ -5,12 +5,37 @@ let colorSelected;
 
 // Add a row
 function addR() {
+
+    let numRows = numRows + 1;
     alert("Clicked Add Row"); // Replace this line with your code.
+
 }
 
 // Add a column
 function addC() {
-    alert("Clicked Add Col"); // Replace this line with your code.
+    let grid = document.getElementById("grid");
+    let numRows = grid.rows.length;
+    if (numRows > 0){
+        numCols = grid.rows[0].cells.length;
+    }
+    else{
+        numRows = 0;
+    } 
+
+    for(let i = 0; i < numRows; i++){ 
+        let new_col = grid.rows[i].insertCell();
+        new_col.onClick = function() {
+            this.style.backgroundColor = colorSelected
+        }
+    }  
+
+    if(numRows === 0){
+        let new_row = grid.insertRow();
+        let new_col = new_row.insertCell();
+        new_col.onclick = function(){
+            this.style.backgroundColor = colorSelected;
+        }
+    }
 }
 
 // Remove a row
